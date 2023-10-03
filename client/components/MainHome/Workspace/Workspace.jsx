5186cde9888163462864314096b7c82579917ff6
ujjwxl <ujjwalk1408@gmail.com>
2023-10-03 20:22:02 +0530

    commit 16
    fixed navbar and sidebar

--- client/components/MainHome/Workspace/Workspace.jsx
+++ client/components/MainHome/Workspace/Workspace.jsx
@@ -5,6 +5,45 @@ export default function Workspace() {
   return (
     <div className='workspace'>
       {/* <h1>Hello</h1> */}
+      <button>Click here to begin!</button>
+      <h2>Welcome</h2>
+      <h2>Welcome</h2>
+      <h2>Welcome</h2>
+      <h2>Welcome</h2>
+      <h2>Welcome</h2>
+      <h2>Welcome</h2>
+      <h2>Welcome</h2>
+      <h2>Welcome</h2>
+      <h2>Welcome</h2>
+      <h2>Welcome</h2>
+      <h2>Welcome</h2>
+      <h2>Welcome</h2>
+      <h2>Welcome</h2>
+      <h2>Welcome</h2>
+      <h2>Welcome</h2>
+      <h2>Welcome</h2>
+      <h2>Welcome</h2>
+      <h2>Welcome</h2>
+      <h2>Welcome</h2>
+      <h2>Welcome</h2>
+      <h2>Welcome</h2>
+      <h2>Welcome</h2>
+      <h2>Welcome</h2>
+      <h2>Welcome</h2>
+      <h2>Welcome</h2>
+      <h2>Welcome</h2>
+      <h2>Welcome</h2>
+      <h2>Welcome</h2>
+      <h2>Welcome</h2>
+      <h2>Welcome</h2>
+      <h2>Welcome</h2>
+      <h2>Welcome</h2>
+      <h2>Welcome</h2>
+      <h2>Welcome</h2>
+      <h2>Welcome</h2>
+      <h2>Welcome</h2>
+      <h2>Welcome</h2>
+      <button>Click here to hehe</button>
     </div>
   )
 }
--- client/pages/MainHome/MainHome.jsx
+++ client/pages/MainHome/MainHome.jsx
@@ -5,13 +5,18 @@ import Workspace from '../../components/MainHome/Workspace/Workspace'
 import './MainHome.css'
 
 export default function MainHome() {
-    return(
+    return (
         <>
-        <Navbar></Navbar>
-        <div className='main-home'> 
-            <SideBar></SideBar>
-            <Workspace></Workspace>
-        </div>
-        </> 
+            <Navbar></Navbar>
+            <div className='main-home'>
+                <div className='home-side-bar'>
+                    <SideBar></SideBar>
+                </div>
+                <div className='home-work-space'>
+                    <Workspace></Workspace>
+                </div>
+
+            </div>
+        </>
     )
 }
